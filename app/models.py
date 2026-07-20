6f82d6d293ede8518bcf11a009be3dd37b374f00
Leandro Carlos <leonpcsn@gmail.com>
2026-07-20 19:18:21 -0300

    fix(auth): ampliar coluna de hash de senha

--- app/models.py
+++ app/models.py
@@ -128,7 +128,7 @@ class User(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
     access_role = db.Column(db.String(40), nullable=True, index=True)
     employee_id = db.Column(
         db.Integer, db.ForeignKey("employees.id"), nullable=True, unique=True, index=True
